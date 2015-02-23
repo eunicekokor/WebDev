f0b3e97d8b6d617ba259f00099574b898612eb87
Eunice Emefa Kokor <eek2138@columbia.edu>
2015-02-23 11:22:48 -0500

    first commit

---
+++ IntroToNode/sync.js
@@ -0,0 +1,7 @@
+console.log("Node starting...");
+
+var fs = require('fs');
+console.log( fs.readFileSync('./file1.txt') );
+// var fs = require('fs');
+// console.log(fs);
+// console.log(fs.readFileSync('./file1.txt'));
---
+++ test_first_javascript/01_properties/properties.js
@@ -0,0 +1,14 @@
+var object = {};
+
+beforeEach(function(){
+	object = {};
+	setSomePropertiesOn(object);
+}
+
+function setSomePropertiesOn(num){
+	object.x = 7;
+	object['y'] = 8;
+	object['onePlus'] = function(num){
+	return (num + 1)
+}
+}
